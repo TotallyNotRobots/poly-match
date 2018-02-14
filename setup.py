39c3f00deb7b2a2030a1a4a190c9737124205fe8
linuxdaemon <linuxdaemon@snoonet.org>
2018-02-14 03:51:41 -0600

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='polymatch',
-    version='0.1.0',
+    version='0.1.1',
     python_requires=">=3.4",
     description="A polymorphic pattern matching library for Python",
     url='https://github.com/TotallyNotRobots/polymatch',
